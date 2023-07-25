6117f6529b905458ffd189761f0091600e3ec9af
Adam Kuruc <me@adamkuruc.dev>
2023-07-25 16:19:43 +0200

    initial folder structure

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import ERC20Token from "./ERC20Token";
+import ERC21Token from "./ERC21Token";
+import ERC1155Token from "./ERC1155Token";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Table from "./Table";
+import Transaction from "./Transaction";
+
+export {
+  ERC20Token,
+  ERC21Token,
+  ERC1155Token,
+  Footer,
+  Navbar,
+  Table,
+  Transaction,
+};
---
+++ context/EtherContext.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const EtherContext = () => {
+  return <div>EtherContext</div>;
+};
