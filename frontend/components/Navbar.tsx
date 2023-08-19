937ba2ea37c3ff2f4383e2043cb52b9557026233
Adam Kuruc <me@adamkuruc.dev>
2023-08-19 22:46:29 +0200

    added searchbar functionality

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -25,28 +25,30 @@ const Navbar = () => {
     }
   };
 
-  console.log(ethPrice);
-
   useEffect(() => {
     getEtherPrice();
   }, []);
 
   return (
     <nav className="flex justify-center px-28 py-6 z-10">
-      <div className="flexStartCenter w-full">
-        <Link href="/" className="text-white font-semibold text-2xl">
-          Ether Explorer
-        </Link>
-        <div className="flex items-center ml-12 text-white">
-          <EtherLogo />
-          <div className="ml-2 font-semibold">
-            ETH{" "}
-            <span className="ml-2 font-normal">
-              ${ethPrice ? ethPrice.toFixed(2) : ""}
-            </span>
-            <span className="ml-1 font-normal text-green-500">(+1.85%)</span>
+      <div className="flex justify-between w-full items-center">
+        <div className="flexStartCenter">
+          <Link href="/" className="text-white font-semibold text-2xl">
+            Ether Explorer
+          </Link>
+          <div className="flex items-center ml-12 text-white">
+            <EtherLogo />
+            <div className="ml-2 font-semibold">
+              ETH{" "}
+              <span className="ml-2 font-normal">
+                ${ethPrice ? ethPrice.toFixed(2) : ""}
+              </span>
+              <span className="ml-1 font-normal text-green-500">(+1.85%)</span>
+            </div>
           </div>
         </div>
+
+        <div className="text-white">Sign in</div>
       </div>
     </nav>
   );
--- frontend/components/Searchbar.tsx
+++ frontend/components/Searchbar.tsx
@@ -1,16 +1,54 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
+import axios from "axios";
+
 import SearchIcon from "@/assets/SearchIcon";
-import React from "react";
 
 const Searchbar = () => {
+  const [showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSearch = async () => {
+    if (!searchInput.trim()) {
+      return;
+    }
+
+    try {
+      const response = await axios.get("http://localhost:5001/address", {
+        params: { address: searchInput },
+      });
+
+      setResult(response.data.result);
+      setShowResult(true);
+    } catch (error) {
+      console.log(`Something went wrong: ${error}`);
+    }
+
+    setSearchInput("");
+  };
+
   return (
     <div className="flex flex-2 flexCenter rounded-2xl bg-ether-grey-3">
       <input
         className="bg-transparent w-full rounded-2xl outline-none text-white p-4 text-sm"
-        placeholder="Search by Address / Request Key / Event / Block Height / Code"
+        placeholder="Search by Address / Txn Hash / Block / Token / Domain Name"
+        onChange={changeHandler}
+        maxLength={120}
+        required
+        value={searchInput}
       />
-      <div className="rounded-2xl m-1 p-4 bg-ether-pink-1 cursor-pointer">
+      <button
+        className="rounded-2xl m-1 p-4 bg-ether-pink-1 cursor-pointer border-0"
+        onClick={handleSearch}
+      >
         <SearchIcon />
-      </div>
+      </button>
     </div>
   );
 };
